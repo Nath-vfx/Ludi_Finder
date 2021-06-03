1a85741ebd3c2aa74ebc0bb275fa9cb9d47fb215
Nathan VAN BIGNOOT <nathan.van-bignoot@outlook.fr>
2021-06-03 21:59:09 +0200

    Commit : landing-page

---
+++ Pages/landing.vue.js
@@ -0,0 +1,55 @@
+var Landing = Vue.component ('Landing', {
+    template: `
+    
+    <section>
+    <img id="femmeDebout" src="elements/img/section_1/Flat-design_femme%20debout.svg" alt="Femme en train de présenter">
+    <img id="nuage" src="elements/img/section_1/Éléments%20décoratifs%201.svg" alt="Nuages">
+    <img id="logo" src="elements/logos/Logo%20expérimental.svg" alt="Logo Ludifinder">
+    <img id="boutonUn" src="elements/logos/bouton-rejoindre.svg" alt="Rejoindre">
+    <img id="textUn" src="elements/texts/text-section-1.svg" alt="">
+
+</section>
+<section>
+    <img id="barreRecherche" src="elements/img/section_2/Flat-design_barre%20de%20recherche.svg"
+         alt="Barres de recherches">
+    <img id="hommeAssis" src="elements/img/section_2/homme-assis.svg" alt="Un home assis">
+    <img id="textDeux" src="elements/texts/text-section-2.svg" alt="Texte de la section 2">
+
+</section>
+<section>
+    <img id="femmeDeux" src="elements/img/section_3/Flat-design_femme2.svg" alt="Femme regardant l'horizon">
+    <img id="hommeDebout" src="elements/img/section_3/Flat-design_homme%20organisé.svg" alt="Homme organisé">
+    <img id="textTrois" src="elements/texts/text-section-3.svg" alt="Texte de la section 3">
+
+</section>
+<section>
+    <img id="montgolfière" src="elements/img/section_4/Flat-design_montgolfière.svg" alt="Montgolfières">
+    <img id="siteBallon" src="elements/img/section_4/Flat-design_ballons%20et%20site%20web.svg"
+         alt="Site transporté par ballon">
+    <img id="nuageDeux" src="elements/img/section_1/Éléments%20décoratifs%201.svg" alt="Nuages">
+    <img id="textQuatre" src="elements/texts/text-section-4.svg" alt="Texte de la section 4">
+
+</section>
+<section>
+    <img src="elements/img/section_5/Flat%20design_personnes%20autour%20d'une%20table.svg" alt="Amis autour d'une table"
+         id="friends">
+    <img src="elements/img/section_5/Flat-design-roi%20et%20reine%20(cartes).svg" alt="Cartes du roi et de la reine"
+         id="queenKing">
+    <img src="elements/texts/text-section-5.svg" alt="Texte de la section 5" id="textCinq">
+
+</section>
+<section>
+    <img id="cloudy" src="elements/img/section_6/Cloudy.svg" alt="Banc de nuages bleues">
+    <img id="play" src="elements/logos/Pret-a-jouer.svg" alt="Pret à jouer ?">
+    <img id="rejoindreDeux" src="elements/logos/bouton-rejoindre.svg" alt="rejoindre">
+
+</section>
+    
+    
+    `,
+    data() {
+        return {}
+    },
+    methods: {}
+
+})
